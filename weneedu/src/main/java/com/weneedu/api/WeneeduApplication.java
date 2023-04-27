b01f8303e4dc25cfbf53634bef9259717010eb34
4ndreiDev <andrei.web.app@gmail.com>
2023-04-27 21:04:44 +0200

    setup project

---
+++ weneedu/src/main/java/com/weneedu/api/WeneeduApplication.java
@@ -0,0 +1,13 @@
+package com.weneedu.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeneeduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeneeduApplication.class, args);
+    }
+
+}
